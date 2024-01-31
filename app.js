a36052e53837d51d6a38719e49acf1299d663891
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-01-31 18:02:32 +0100

    Using the Native Event Object

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
   },
   methods: {
@@ -14,6 +15,9 @@ const app = Vue.createApp({
       } else {
         return 0;
       }
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
     },
   },
 });
