9fdbd71e36df0dc56e27cd03d13573af50b148df
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-02-02 13:04:00 +0100

    Working with Watchers

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 10,
       name: "",
+      lastName: "",
       confirmedName: "",
+      // fullname: "",
     };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
+      }
+    },
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = value + "" + this.lastName;
+    //   }
+    // },
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = this.name + "" + value;
+    //   }
+    // },
   },
   computed: {
     fullname() {
-      if (this.name === "") {
+      if (this.name === "" || this.lastName === "") {
         return "";
       }
-      return this.name + " " + "Nachname";
+      return this.name + " " + this.lastName;
     },
   },
   methods: {
