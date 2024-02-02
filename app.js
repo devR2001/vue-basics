b0cc0e5c318e1c55170d302f5379622187589327
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-02-02 20:09:45 +0100

    Rendering Content Conditionally

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
-      boxASelected: false,
-      boxBSelected: false,
-      boxCSelected: false,
+      enteredGoalValue: "",
+      goals: [],
     };
   },
-  computed: {
-    boxAClasses() {
-      return { active: this.boxASelected };
-    },
-  },
   methods: {
-    boxSelected(box) {
-      if (box === "A") {
-        this.boxASelected = !this.boxASelected;
-      } else if (box === "B") {
-        this.boxBSelected = !this.boxBSelected;
-      } else if (box === "C") {
-        this.boxCSelected = !this.boxCSelected;
-      }
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
     },
   },
 });
 
-app.mount("#styling");
+app.mount("#user-goals");
