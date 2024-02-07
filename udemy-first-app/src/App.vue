ef6a6dd27d7b09cdbc7a3a3b95d87b7b7be3bcb6
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-02-07 13:16:02 +0100

    udemy setup

--- udemy-first-app/src/App.vue
+++ udemy-first-app/src/App.vue
@@ -1,29 +1,30 @@
 <template>
-  <header><h1>My Friends</h1></header>
-  <ul>
-    <FriendContactVue />
-  </ul>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
 </template>
 
 <script>
-import FriendContactVue from "./components/FriendContact.vue";
 export default {
-  components: {
-    FriendContactVue,
-  },
   data() {
     return {
       friends: [
         {
           id: "manuel",
           name: "Manuel Lorenz",
-          phone: "01234 435345 233",
+          phone: "0123 45678 90",
           email: "manuel@localhost.com",
         },
         {
           id: "julie",
           name: "Julie Jones",
-          phone: "01234 435345 233",
+          phone: "0987 654421 21",
           email: "julie@localhost.com",
         },
       ],
@@ -36,15 +37,12 @@ export default {
 * {
   box-sizing: border-box;
 }
-
 html {
   font-family: "Jost", sans-serif;
 }
-
 body {
   margin: 0;
 }
-
 header {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 3rem auto;
@@ -56,13 +54,11 @@ header {
   width: 90%;
   max-width: 40rem;
 }
-
 #app ul {
   margin: 0;
   padding: 0;
   list-style: none;
 }
-
 #app li {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
@@ -72,14 +68,12 @@ header {
   width: 90%;
   max-width: 40rem;
 }
-
 #app h2 {
   font-size: 2rem;
   border-bottom: 4px solid #ccc;
   color: #58004d;
   margin: 0 0 1rem 0;
 }
-
 #app button {
   font: inherit;
   cursor: pointer;
@@ -89,7 +83,6 @@ header {
   padding: 0.05rem 1rem;
   box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
 }
-
 #app button:hover,
 #app button:active {
   background-color: #ec3169;
--- udemy-first-app/src/components/FriendContact.vue
+++ udemy-first-app/src/components/FriendContact.vue
@@ -1,12 +1,16 @@
 <template>
   <li>
-    <h2>
-      {{ friend.name }}
-    </h2>
-    <button @click="toggleDetails">Show Details</button>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
-      <li><strong>Phone:</strong>{{ friend.phone }}</li>
-      <li><strong>Email:</strong>{{ friend.email }}</li>
+      <li>
+        <strong>Phone:</strong>
+        {{ friend.phone }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ friend.email }}
+      </li>
     </ul>
   </li>
 </template>
@@ -19,7 +23,7 @@ export default {
       friend: {
         id: "manuel",
         name: "Manuel Lorenz",
-        phone: "01234 435345 233",
+        phone: "0123 45678 90",
         email: "manuel@localhost.com",
       },
     };
@@ -27,9 +31,7 @@ export default {
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
-    },
-  },
+    }
+  }
 };
 </script>
-
-<style></style>
