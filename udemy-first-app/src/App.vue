80272746b572183172c2e412dbf4250580930300
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-02-07 13:36:01 +0100

    Prop Behavior & Changing Props

--- udemy-first-app/src/App.vue
+++ udemy-first-app/src/App.vue
@@ -8,11 +8,13 @@
         name="Manuel Lorenz"
         phone-number="1234 78992"
         email-address="manuel@localhost.com"
+        is-favorite="1"
       ></friend-contact>
       <friend-contact
         name="Julie Jones"
         phone-number="1234 78992"
         email-address="julie@localhost.com"
+        is-favorite="0"
       ></friend-contact>
     </ul>
   </section>
