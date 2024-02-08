cbd7c3e0eaf396b618f15feedef400c5b896f469
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-02-08 12:51:19 +0100

    Defining & Validating Custom Events

--- udemy-first-app/src/components/FriendContact.vue
+++ udemy-first-app/src/components/FriendContact.vue
@@ -47,6 +47,16 @@ export default {
       },
     },
   },
+  emits: ["toggle-favorite"],
+  // emits: {
+  //   "toggle-favorite": function (id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn("Id is missing");
+  //     }
+  //   },
+  // },
   data() {
     return {
       detailsAreVisible: false,
